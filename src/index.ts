d66c2fdeb17915fc86bcfd2702e4f6c556709c88
Svyatanchyk <svyatan95@gmail.com>
2025-05-03 14:57:22 +0300

    fix cors issues #2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import connectDB from "./config/db";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
 import { router } from "./routes/router";
 import cookieParser from "cookie-parser";
@@ -12,12 +12,25 @@ const CLIENT_URL = process.env.FRONTEND_BASE_URL;
 
 app.use(cookieParser());
 app.use(express.json());
-const corsOptions = {
-  origin: "*", // Allow all origins
-  credentials: true, // Allow cookies and authentication headers if needed
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://generise.netlify.app",
+    ];
+
+    if (allowedOrigins.includes(origin!) || !origin) {
+      // Allow requests from allowed origins or if no origin is provided (e.g., for local testing)
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Allow credentials (cookies, HTTP authentication)
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
 };
 
+// Apply the CORS configuration
 app.use(cors(corsOptions));
 
 app.use(router);
